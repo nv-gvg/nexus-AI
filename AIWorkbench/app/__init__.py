9ce5e56b36e732e7a7a098bae542b00698c2232e
nv-gvg <19589860701@163.com>
2026-08-21 22:56:37 +0800

    Delete AIWorkbench directory

--- AIWorkbench/app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""AIWorkbench 应用包。"""
-
-from .application import create_application
-
-__all__ = ["create_application"]
--- AIWorkbench/app/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""UI 子包。"""
-
-from .main_window import MainWindow
-
-__all__ = ["MainWindow"]
